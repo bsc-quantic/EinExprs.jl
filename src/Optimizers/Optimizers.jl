08f33d3ae2c8022b93a45972c6bce32ef43c6d42
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-03-30 12:19:27 +0200

    Fix name of legacy abstract type

--- src/Optimizers/Optimizers.jl
+++ src/Optimizers/Optimizers.jl
@@ -2,8 +2,8 @@ abstract type Optimizer end
 
 function einexpr end
 
-einexpr(T::Type{<:Solver}, args...; kwargs...) = einexpr(T(; kwargs...), args...)
-einexpr(config::Solver, output, inputs, size_dict) = einexpr(config, ...) # TODO
+einexpr(T::Type{<:Optimizer}, args...; kwargs...) = einexpr(T(; kwargs...), args...)
+einexpr(config::Optimizer, output, inputs, size_dict) = einexpr(config, ...)
 
 include("Exhaustive.jl")
 include("Greedy.jl")
