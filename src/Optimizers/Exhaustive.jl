2a54bea60cf8730693baec13edcb01efe1f1473b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-03-30 17:00:12 +0200

    Refactor `Exhaustive` optimizer for new `EinExpr` type

--- src/Optimizers/Exhaustive.jl
+++ src/Optimizers/Exhaustive.jl
@@ -9,25 +9,31 @@ Exhaustive contraction path optimizers. It guarantees to find the optimal contra
     outer::Bool = false
 end
 
-function einexpr(config::Exhaustive, einexpr)
+function einexpr(config::Exhaustive, output, inputs)
     config.outer && throw("Exhaustive search with outer-products is not implemented yet")
 
-    flops_best = Ref(typemax(Int128))
-    __einexpr_exhaustive(einexpr, flops_best)
+    expr = EinExpr(inputs, output)
+    flops_best = Ref(flops(expr))
+    __einexpr_exhaustive(expr, flops_best)
 end
 
-function __einexpr_exhaustive(path, flops_best)
-    # TODO depth-first search of paths
-    for indices in uncontractedinds(path)
-        # TODO get (intermediate) tensors containing such index
+function __einexpr_exhaustive(expr, flops_best)
+    # TODO group indices if they are parallel
+    indices = setdiff(vcat(labels.(expr.args)...), expr.head)
 
-        # TODO add einsum node of tensor contraction of such index (and its parallels?)
-        candidate = copy(path)
-        node = Expr(:call, :einsum, indices)
-        push!(candidate, node)
+    for index in indices
+        # select tensors containing such index
+        targets = filter(∋(index) ∘ labels, expr.args)
+        target_inds = labels.(targets)
+        contracting_inds = setdiff(∩(target_inds...), expr.head)
+        output_inds = setdiff(unique(vcat(target_inds)), contracting_inds)
+
+        # add einsum node of tensor contraction of such index (and its parallels?)
+        node = EinExpr(targets, output_inds)
+        path = EinExpr([node, filter(∌(index) ∘ labels, expr.args)...], expr.head)
 
         # prune paths based on flops
-        flops(candidate) >= flops_best[] && continue
+        flops(path) >= flops_best[] && continue
 
         # TODO prune paths based on memory limit?
 
