3a48fbad5c15c2abbdf36f92bb628a617686c29d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-03-30 12:20:47 +0200

    Fix generic interface of `einexpr(::Optimizer)`

--- src/Optimizers/Optimizers.jl
+++ src/Optimizers/Optimizers.jl
@@ -3,7 +3,7 @@ abstract type Optimizer end
 function einexpr end
 
 einexpr(T::Type{<:Optimizer}, args...; kwargs...) = einexpr(T(; kwargs...), args...)
-einexpr(config::Optimizer, output, inputs, size_dict) = einexpr(config, ...)
+einexpr(config::Optimizer, output, inputs) = einexpr(config, output, inputs)
 
 include("Exhaustive.jl")
 include("Greedy.jl")
