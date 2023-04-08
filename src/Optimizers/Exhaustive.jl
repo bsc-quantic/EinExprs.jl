78d83a0e30fbc64f94858fe622010a2b71316593
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-04-08 10:20:35 +0200

    Generalize `metric` in `Exhaustive` optimizer

--- src/Optimizers/Exhaustive.jl
+++ src/Optimizers/Exhaustive.jl
@@ -6,6 +6,7 @@ using Base: @kwdef
 Exhaustive contraction path optimizers. It guarantees to find the optimal contraction path but at the cost of ``\\mathcal{O}(n!)`` time complexity.
 """
 @kwdef struct Exhaustive <: Optimizer
+    metric::Function = flops
     outer::Bool = false
 end
 
@@ -13,7 +14,7 @@ function einexpr(config::Exhaustive, expr; leader=expr)
     config.outer && throw("Exhaustive search with outer-products is not implemented yet")
 
     if length(suminds(expr, parallel=true)) == 1
-        return flops(expr) < flops(leader) ? expr : leader
+        return config.metric(expr) < config.metric(leader) ? expr : leader
     end
 
     # TODO type annotation in `suminds(expr, parallel=true)::Vector{Vector{Symbol}}` for type-inference?
@@ -28,10 +29,8 @@ function einexpr(config::Exhaustive, expr; leader=expr)
 
         candidate = EinExpr(targets, suboutput)
 
-        # prune paths based on flops
-        flops(candidate) >= flops(leader) && continue
-
-        # TODO prune paths based on memory limit?
+        # prune paths based on config.metric
+        config.metric(candidate) >= config.metric(leader) && continue
 
         # recurse fixing candidate index
         candidate = EinExpr([
