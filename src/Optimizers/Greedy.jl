7dcee5f29d3d9904ac3a628a54868af35ff74783
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-04-13 18:44:51 +0200

    Implement `Greedy` optimizer

--- src/Optimizers/Greedy.jl
+++ src/Optimizers/Greedy.jl
@@ -1,4 +1,5 @@
 using Base: @kwdef
+using DataStructures: MutableBinaryHeap, update!
 
 """
     Greedy
@@ -6,11 +7,51 @@ using Base: @kwdef
 Greedy contraction path solver. Greedily selects contractions that maximize the metric.
 """
 @kwdef struct Greedy <: Optimizer
-    choose::Function
     metric::Function = removedsize
+    choose::Function = pop!
 end
 
 function einexpr(config::Greedy, expr)
-    # TODO memory limit?
-end
+    # generate initial candidate contractions
+    queue = MutableBinaryHeap{Tuple{Float64,Vector{Symbol}}}(Base.By(first))
 
+    handles = map(suminds(expr, parallel=true)) do inds
+        candidate = sum(select(expr, inds)..., inds=inds)
+        weight = config.metric(candidate)
+        handle = push!(queue, (weight, inds))
+        return sort(inds) => handle
+    end |> Dict
+
+    while length(queue) > 1
+        # select candidate
+        _, winner = config.choose(queue)
+        any(∉(suminds(expr)), winner) && continue
+
+        # append winner to contraction path
+        neigh = neighbours(expr, winner)
+
+        sum!(expr, winner)
+
+        # update candidate queue
+        for inds in filter(inds -> !isdisjoint(neigh, inds), suminds(expr, parallel=true))
+            candidate = sum(select(expr, inds)..., inds=inds)
+            weight = config.metric(candidate)
+
+            # update involved nodes
+            if inds ∈ keys(handles)
+                update!(queue, handles[sort(inds)], (weight, inds))
+            else
+                # if new parallel indices have appeared, delete old nodes and create new ones
+                for key in Iterators.map(sort, Iterators.filter(key -> !isdisjoint(inds, key), keys(handles)))
+                    # key = sort(key)
+                    delete!(queue, handles[key])
+                    delete!(handles, key)
+                end
+
+                handles[sort(inds)] = push!(queue, (weight, inds))
+            end
+        end
+    end
+
+    return expr
+end
