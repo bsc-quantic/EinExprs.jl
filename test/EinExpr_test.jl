6bdbfbffefa3249855d1563d8a137fad6664b193
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-04-17 11:27:31 +0200

    Test manual fixing of contraction path

--- test/EinExpr_test.jl
+++ test/EinExpr_test.jl
@@ -179,4 +179,48 @@
         @test suminds(expr) == [:k]
         @test suminds(expr, parallel=true) == [[:k]]
     end
+
+    @testset "manual path" begin
+        sizes = Dict(
+            :o => 2,
+            :b => 2,
+            :p => 2,
+            :n => 2,
+            :j => 2,
+            :k => 2,
+            :d => 2,
+            :e => 2,
+            :c => 2,
+            :h => 2,
+            :i => 2,
+            :l => 2,
+            :m => 2,
+            :q => 2,
+            :a => 2,
+            :f => 2,
+            :g => 2,
+        )
+
+        tensors = [
+            Tensor(ones((sizes[i] for i in [:f, :l, :i])...), [:f, :l, :i]),
+            Tensor(ones((sizes[i] for i in [:b, :e])...), [:b, :e]),
+            Tensor(ones((sizes[i] for i in [:g, :n, :l, :a])...), [:g, :n, :l, :a]),
+            Tensor(ones((sizes[i] for i in [:o, :i, :m, :c])...), [:o, :i, :m, :c]),
+            Tensor(ones((sizes[i] for i in [:k, :d, :h, :a, :n, :j])...), [:k, :d, :h, :a, :n, :j]),
+            Tensor(ones((sizes[i] for i in [:m, :f, :q])...), [:m, :f, :q]),
+            Tensor(ones((sizes[i] for i in [:p, :k])...), [:p, :k]),
+            Tensor(ones((sizes[i] for i in [:c, :e, :h])...), [:c, :e, :h]),
+            Tensor(ones((sizes[i] for i in [:g, :q])...), [:g, :q]),
+            Tensor(ones((sizes[i] for i in [:d, :b, :o])...), [:d, :b, :o]),
+        ]
+
+        expr = EinExpr(tensors, [:p, :j])
+
+        @test sum(tensors..., inds=[:p, :j]) == expr
+
+        for inds in [[:q], [:m], [:f, :i], [:g, :l], [:b], [:o], [:c, :e], [:n, :a, :d, :h], [:k]]
+            foo = sum(expr, inds)
+            @test foo == sum!(expr, inds)
+        end
+    end
 end
