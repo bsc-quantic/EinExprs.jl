810c8ec152a3cfda1c7227948354f795957b3ba6
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2023-06-21 16:53:20 +0200

    Define forward and backward differentiation rules for `contract` on `EinExpr` (#17)
    * Init `ChainRulesCore` package extension
    
    * Implement `frule` for `contract` method
    
    * Fix integration with `FiniteDifferences.to_vec` with Package Extension
    
    * Define forward derivation rules for `contract`
    
    * Move `FiniteDifferences` from `weakdeps` to `deps`
    
    * Fix `FiniteDifferences` weak dependency
    
    * Implement `rrule` for `contract`
    
    * Fix `rrule`
    
    * Test matrix multiplication
    
    * Fix typo in test
    
    * Fix `rrule` on trace
    
    * Fix `rrule` of `contract` on products
    
    * Fix `rrule` of `contract` on diagonal
    
    ---------
    
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
    Co-authored-by: Ema Puljak <ema.puljak@cern.ch>

---
+++ ext/EinExprsFiniteDifferencesExt.jl
@@ -0,0 +1,25 @@
+module EinExprsFiniteDifferencesExt
+
+if isdefined(Base, :get_extension)
+    using EinExprs
+else
+    using ..EinExprs
+end
+
+using FiniteDifferences
+
+function FiniteDifferences.to_vec(expr::EinExpr)
+    x_vecs_and_backs = map(to_vec, expr.args)
+    x_vecs, x_backs = first.(x_vecs_and_backs), last.(x_vecs_and_backs)
+    lengths = map(length, x_vecs)
+    sz = typeof(lengths)(cumsum(collect(lengths)))
+    function EinExpr_from_vec(v)
+        args = map(x_backs, lengths, sz) do x_back, l, s
+            return x_back(v[s-l+1:s])
+        end
+        EinExpr(args, expr.head)
+    end
+    return reduce(vcat, x_vecs), EinExpr_from_vec
+end
+
+end
--- src/EinExprs.jl
+++ src/EinExprs.jl
@@ -18,6 +18,8 @@ end
 @static if !isdefined(Base, :get_extension)
     function __init__()
         @require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("../ext/EinExprsMakieExt.jl")
+        @require FiniteDifferences = "26cc04aa-876d-5657-8c51-4c34ba976000" include("../ext/EinExprsFiniteDifferencesExt.jl")
+        @require ChainRulesCore = "d360d2e6-b24c-11e9-a2a3-2a2ae2dbcce4" include("../ext/EinExprsChainRulesCoreExt.jl")
     end
 end
 
