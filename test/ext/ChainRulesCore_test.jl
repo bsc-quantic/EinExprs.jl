02c28e250085096b765fb2b7b0b0970a7b783d7d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-06-21 11:23:01 +0200

    Test matrix multiplication

--- test/ext/ChainRulesCore_test.jl
+++ test/ext/ChainRulesCore_test.jl
@@ -41,6 +41,10 @@
             test_rrule(contract, expr, check_inferred=false, check_thunked_output_tangent=false)
         end
 
-        # TODO matrix multiplication
+        @testset "matrix multiplication" begin
+            expr = EinExpr([Tensor(rand(2, 3), (:i, :k)), Tensor(rand(3, 4), (:k, :j))])
+            test_frule(contract, expr)
+            test_rrule(contract, expr, check_inferred=false, check_thunked_output_tangent=false)
+        end
     end
 end
