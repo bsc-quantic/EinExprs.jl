b885a643e6d22014bd374461e42f1502e780c29b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-07-21 17:08:29 +0200

    Refactor `flops` counter

--- src/Counters.jl
+++ src/Counters.jl
@@ -1,15 +1,7 @@
 using Tensors: Tensor
-using Memoize
 
 flops(::Tensor) = 0
-@memoize function flops(expr::EinExpr)
-    flops_sub = sum(flops.(expr.args))
-
-    floppi(inds) = mapreduce(i -> size(expr, i), *, inds, init = one(BigInt))
-    flops_cur = floppi(suminds(expr)) * (isempty(suminds(expr)) && length(expr.args) == 1 ? 0 : floppi(labels(expr)))
-
-    return flops_sub + flops_cur
-end
+flops(expr::EinExpr) = mapreduce(i -> size(expr, i), *, [labels(expr)..., suminds(expr)...])
 
 removedsize(::Tensor) = 0
 removedsize(expr::EinExpr) = mapreduce(prod ∘ size, +, expr.args) - prod(size(expr))
