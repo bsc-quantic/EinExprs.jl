af67428c45e2a61f096d276045fe07c64857c056
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-08-27 00:32:20 +0200

    Update `flops`, `Exhaustive` to new `AbstractTrees` integration

--- src/Counters.jl
+++ src/Counters.jl
@@ -1,5 +1,5 @@
 flops(expr::EinExpr) =
-    if isempty(suminds(expr)) && length(expr.args) == 1
+    if length(expr.args) == 0
         0
     else
         mapreduce(i -> size(expr, i), *, [head(expr)..., suminds(expr)...], init = one(BigInt))
