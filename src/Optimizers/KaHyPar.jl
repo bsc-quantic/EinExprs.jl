87246cbed048a10e5274b8ad24ecc2a25c283363
Jofre Vallès <jofrevalles99@gmail.com>
2023-10-27 10:39:29 +0200

    Create incidence matrix via subviews directly

--- src/Optimizers/KaHyPar.jl
+++ src/Optimizers/KaHyPar.jl
@@ -13,10 +13,19 @@ function EinExprs.einexpr(config::HyPar, path)
     inds = mapreduce(head, ∪, path.args)
     indexmap = Dict(Iterators.map(splat(Pair) ∘ reverse, enumerate(inds)))
 
-    I = Iterators.flatmap(((i, tensor),) -> fill(i, ndims(tensor)), enumerate(path.args)) |> collect
-    J = Iterators.flatmap(tensor -> Iterators.map(Base.Fix1(getindex, indexmap), head(tensor)), path.args) |> collect
-    V = fill(1, length(I))
-    incidence_matrix = sparse(I, J, V)
+    num_columns = maximum(values(indexmap))
+    num_rows = length(path.args)
+    incidence_matrix = spzeros(Int, num_rows, num_columns)
+
+    # Iterate through each tensor and its associated indices, and update the incidence matrix.
+    for (i, tensor) in enumerate(path.args)
+        tensor_indices = [i]  # Current tensor is represented as a row in the matrix.
+        edge_indices = [indexmap[idx] for idx in head(tensor)]  # Map indices via 'indexmap'.
+
+        # Create a subview for the current tensor and associated hyperedges.
+        incidence_subview = view(incidence_matrix, tensor_indices, edge_indices)
+        incidence_subview .= 1  # Update the subview directly. This step modifies the original sparse matrix.
+    end
 
     # NOTE indices in `inds` should be in the same order as unique indices appear by iterating on `path.args` because `∪` retains order
     edge_weights = map(Base.Fix1(size, path), inds)
