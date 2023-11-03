c92f8bfa798155e6e31a6c787ac2be38bd9f53b1
Jofre Vallès <jofrevalles99@gmail.com>
2023-11-03 10:24:45 +0100

    Fix code from code review

--- src/Optimizers/KaHyPar.jl
+++ src/Optimizers/KaHyPar.jl
@@ -7,8 +7,8 @@ using KaHyPar
     imbalance::Float32 = 0.03
     stop::Function = <=(2) ∘ length ∘ Base.Fix1(getfield, :args)
     configuration::Union{Nothing,Symbol,String} = nothing
-    edge_scaler::Function = *(1000) ∘ Int ∘ round ∘ log2
-    vertex_scaler::Function = *(1000) ∘ Int ∘ round ∘ log2
+    edge_scaler::Function = Base.Fix1(*, 1000) ∘ Int ∘ round ∘ log2
+    vertex_scaler::Function = Base.Fix1(*, 1000) ∘ Int ∘ round ∘ log2
 end
 
 function EinExprs.einexpr(config::HyPar, path)
