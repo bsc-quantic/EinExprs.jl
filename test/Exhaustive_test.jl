0766f7684e8fae2a21fc26f65eb14f4520de8ab3
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-01-18 11:44:29 +0100

    Fix tests for Exhaustive strategies

--- test/Exhaustive_test.jl
+++ test/Exhaustive_test.jl
@@ -1,36 +1,62 @@
 @testset "Exhaustive" begin
-    sizedict = Dict(i => 2 for i in [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j])
-    tensors = [
-        EinExpr([:j, :b, :i, :h]),
-        EinExpr([:a, :c, :e, :f]),
-        EinExpr([:j]),
-        EinExpr([:e, :a, :g]),
-        EinExpr([:f, :b]),
-        EinExpr([:i, :h, :d]),
-        EinExpr([:d, :g, :c]),
-    ]
-    expr = EinExpr(Symbol[], tensors)
-    sexpr = SizedEinExpr(expr, sizedict)
+    @testset "Depth-first" begin
+        sizedict = Dict(i => 2 for i in [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j])
+        tensors = [
+            EinExpr([:j, :b, :i, :h]),
+            EinExpr([:a, :c, :e, :f]),
+            EinExpr([:j]),
+            EinExpr([:e, :a, :g]),
+            EinExpr([:f, :b]),
+            EinExpr([:i, :h, :d]),
+            EinExpr([:d, :g, :c]),
+        ]
+        expr = EinExpr(Symbol[], tensors)
+        sexpr = SizedEinExpr(expr, sizedict)
 
-    path = einexpr(Exhaustive, sexpr)
+        path = einexpr(Exhaustive(strategy = :depth), sexpr)
 
-    @test path isa SizedEinExpr
+        @test path isa SizedEinExpr
 
-    @test mapreduce(flops, +, Branches(path)) == 92
+        @test mapreduce(flops, +, Branches(path)) == 92
 
-    @test all(splat(issetequal), zip(contractorder(path), [[:a, :e], [:c, :g], [:f], [:j], [:i, :h], [:d, :b]]))
+        @test all(splat(issetequal), zip(contractorder(path), [[:a, :e], [:c, :g], [:f], [:j], [:i, :h], [:d, :b]]))
 
-    @testset "hyperedges" begin
-        sizedict = Dict(i => 2 for i in [:i, :j, :k, :l, :m, :β])
-        a = EinExpr([:i, :β, :j])
-        b = EinExpr([:k, :β])
-        c = EinExpr([:β, :l, :m])
+        @testset "hyperedges" begin
+            sizedict = Dict(i => 2 for i in [:i, :j, :k, :l, :m, :β])
+            a = EinExpr([:i, :β, :j])
+            b = EinExpr([:k, :β])
+            c = EinExpr([:β, :l, :m])
 
-        path = einexpr(EinExprs.Exhaustive(), SizedEinExpr(sum([a, b, c], skip = [:β]), sizedict))
-        @test all(∋(:β) ∘ head, branches(path))
+            path = einexpr(EinExprs.Exhaustive(strategy = :depth), SizedEinExpr(sum([a, b, c], skip = [:β]), sizedict))
+            @test all(∋(:β) ∘ head, branches(path))
 
-        path = einexpr(EinExprs.Exhaustive(), SizedEinExpr(sum([a, b, c], skip = Symbol[]), sizedict))
-        @test all(∋(:β) ∘ head, branches(path)[1:end-1])
-        @test all(!∋(:β) ∘ head, branches(path)[end:end])
+            path =
+                einexpr(EinExprs.Exhaustive(strategy = :depth), SizedEinExpr(sum([a, b, c], skip = Symbol[]), sizedict))
+            @test all(∋(:β) ∘ head, branches(path)[1:end-1])
+            @test all(!∋(:β) ∘ head, branches(path)[end:end])
+        end
+    end
+
+    @testset "Breadth-first" begin
+        sizedict = Dict(i => 2 for i in [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j])
+        tensors = [
+            EinExpr([:j, :b, :i, :h]),
+            EinExpr([:a, :c, :e, :f]),
+            EinExpr([:j]),
+            EinExpr([:e, :a, :g]),
+            EinExpr([:f, :b]),
+            EinExpr([:i, :h, :d]),
+            EinExpr([:d, :g, :c]),
+        ]
+        expr = EinExpr(Symbol[], tensors)
+        sexpr = SizedEinExpr(expr, sizedict)
+
+        path = einexpr(Exhaustive(strategy = :breadth), sexpr)
+
+        @test path isa SizedEinExpr
+
+        @test mapreduce(flops, +, Branches(path)) == 90
+
+        @test all(splat(issetequal), zip(contractorder(path), [[:a, :e], [:c, :g], [:f], [:d], [:b, :i, :h], [:j]]))
     end
 end
