0700b4896857dd936f8998fc410c72ade6ca980d
Todorbsc <todor.krasimirov@bsc.es>
2024-02-09 19:25:44 +0100

    Separate optimization for each disconnected components in path

--- src/Optimizers/Optimizers.jl
+++ src/Optimizers/Optimizers.jl
@@ -3,7 +3,7 @@ abstract type Optimizer end
 function einexpr end
 
 einexpr(T::Type{<:Optimizer}, args...; kwargs...) = einexpr(T(; kwargs...), args...)
-einexpr(config::Optimizer, expr, sizedict) = einexpr(config, SizedEinExpr(expr, sizedict))
+einexpr(config::Optimizer, expr, sizedict) = sum(einexpr.((config,), [SizedEinExpr(exp, sizedict) for exp in [comp for comp in components(expr)]]))
 
 include("Naive.jl")
 include("Exhaustive.jl")
