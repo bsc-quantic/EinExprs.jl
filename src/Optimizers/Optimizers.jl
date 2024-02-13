2fc3371d7ec50fb281f1ea9f32cced42bbdaff3c
Todorbsc <todor.krasimirov@bsc.es>
2024-02-13 13:46:02 +0100

    Fix code style

--- src/Optimizers/Optimizers.jl
+++ src/Optimizers/Optimizers.jl
@@ -3,7 +3,7 @@ abstract type Optimizer end
 function einexpr end
 
 einexpr(T::Type{<:Optimizer}, args...; kwargs...) = einexpr(T(; kwargs...), args...)
-einexpr(config::Optimizer, expr, sizedict) = sum(einexpr.((config,), [SizedEinExpr(exp, sizedict) for exp in [comp for comp in components(expr)]]))
+einexpr(config::Optimizer, expr, sizedict) = sum(einexpr.((config,), map(exp -> SizedEinExpr(exp, sizedict), components(expr))))
 
 include("Naive.jl")
 include("Exhaustive.jl")
