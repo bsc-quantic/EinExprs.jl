dcf9808488c3f098df7ca9a31d53dad697f84e0d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-03-28 20:29:57 +0100

    Set more functions as non-differentiable

--- ext/EinExprsChainRulesCoreExt.jl
+++ ext/EinExprsChainRulesCoreExt.jl
@@ -3,6 +3,23 @@ module EinExprsChainRulesCoreExt
 using EinExprs
 using ChainRulesCore
 
-@non_differentiable einexpr(::Any...)
+for f in [
+    :head,
+    :args,
+    :nargs,
+    :inds,
+    :branches,
+    :leaves,
+    :suminds,
+    :parsuminds,
+    :einexpr,
+    :sumtraces,
+    :indshistogram,
+    :hyperinds,
+    :neighbours,
+    :select,
+]
+    @eval @non_differentiable EinExprs.$f(::Any...)
+end
 
 end
