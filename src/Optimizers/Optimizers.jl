b2b60eedd3cc0424c0e50651b29ac2d98fd81230
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-06-21 00:53:09 +0200

    Add `einexpr` method with canonization

--- src/Optimizers/Optimizers.jl
+++ src/Optimizers/Optimizers.jl
@@ -5,6 +5,15 @@ function einexpr end
 einexpr(T::Type{<:Optimizer}, args...; kwargs...) = einexpr(T(; kwargs...), args...)
 einexpr(config::Optimizer, expr, sizedict) = einexpr(config, SizedEinExpr(expr, sizedict))
 
+function einexpr(path::SizedEinExpr; optimizer)
+    path = deepcopy(path)
+
+    # remove inds with dim=1 (shadow inds)
+    canonize!(SumShadowInds(), path)
+
+    einexpr(optimizer, path)
+end
+
 include("Naive.jl")
 include("Exhaustive.jl")
 include("Greedy.jl")
