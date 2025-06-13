ce9ec59b29f769a1a795988ff4ba81aeec96d6b4
Richard Samuelson <samuelson.rick@gmail.com>
2025-06-13 10:35:51 -0400

    Rewrite optimizer `HyPar`.

--- ext/EinExprsKaHyParExt.jl
+++ ext/EinExprsKaHyParExt.jl
@@ -1,46 +1,36 @@
 module EinExprsKaHyParExt
 
+using CliqueTrees
 using EinExprs
-using AbstractTrees
-using SparseArrays
-using KaHyPar
-using Suppressor
-using Compat
+
+function score(path::SizedEinExpr)
+    return log2(mapreduce(flops, +, Branches(path)))
+end
 
 function EinExprs.einexpr(config::EinExprs.HyPar, path)
-    config.stop(path) && return path
-
-    inds = mapreduce(head, ∪, path.args)
-    indexmap = Dict(Iterators.map(@compat(splat(Pair)) ∘ reverse, enumerate(inds)))
-
-    I = EinExprs.flatmap(((i, tensor),) -> fill(i, ndims(tensor)), enumerate(path.args)) |> collect
-    J = EinExprs.flatmap(tensor -> Iterators.map(Base.Fix1(getindex, indexmap), head(tensor)), path.args) |> collect
-    V = fill(1, length(I))
-    incidence_matrix = sparse(I, J, V)
-
-    # NOTE indices in `inds` should be in the same order as unique indices appear by iterating on `path.args` because `∪` retains order
-    edge_weights = map(config.edge_scaler ∘ Base.Fix1(size, path), inds)
-    vertex_weights = map(config.vertex_scaler ∘ length, args(path))
-
-    hypergraph = KaHyPar.HyperGraph(incidence_matrix, vertex_weights, edge_weights)
-    KaHyPar.kahypar_set_seed(hypergraph.context, config.seed)
-
-    partitions = @suppress KaHyPar.partition(
-        hypergraph,
-        config.parts;
-        imbalance = config.imbalance,
-        configuration = config.configuration,
+    algs = (
+        MF(),
+        MMD(),
     )
 
-    _args = map(unique(partitions)) do partition
-        selection = partitions .== partition
-        count(selection) == 1 && return only(args(path)[selection])
+    diss = (
+        KaHyParND(),
+    )
 
-        expr = sum(args(path)[selection], skip = path.head)
-        einexpr(config, expr)
+    imbalances = config.imbalances
+    minpath = nothing; minscore = typemax(Float64)
+
+    for alg in algs, dis in diss, imbalance in imbalances
+        curconfig = LineGraph(SafeRules(ND(alg, dis; imbalance)))
+        curpath = einexpr(curconfig, path)
+        curscore = score(curpath)
+
+        if curscore < minscore
+            minpath, minscore = curpath, curscore
+        end
     end
 
-    return sum(_args, skip = path.head)
+    return minpath
 end
 
 end
--- src/Optimizers/KaHyPar.jl
+++ src/Optimizers/KaHyPar.jl
@@ -1,11 +1,5 @@
 using Base: @kwdef
 
 @kwdef struct HyPar <: Optimizer
-    parts::Int = 2
-    imbalance::Float32 = 0.03
-    stop::Function = <=(2) ∘ length ∘ Base.Fix2(getproperty, :args)
-    configuration::Union{Nothing,Symbol,String} = nothing
-    edge_scaler::Function = Base.Fix1(*, 1000) ∘ Int ∘ round ∘ log2
-    vertex_scaler::Function = Base.Fix1(*, 1000) ∘ Int ∘ round ∘ log2
-    seed::Int = 0
+    imbalances::StepRange{Int, Int} = 130:130
 end
