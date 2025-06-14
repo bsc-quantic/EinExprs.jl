18ece143d3e2765781e44bd561198ffef538364b
Richard Samuelson <samuelson.rick@gmail.com>
2025-06-13 20:47:44 -0400

    Update docs.

---
+++ docs/src/optimizers/hy_par.md
@@ -0,0 +1,5 @@
+# Hypergraph partitioning optimizer
+
+```@docs
+EinExprs.HyPar
+```
--- src/Optimizers/KaHyPar.jl
+++ src/Optimizers/KaHyPar.jl
@@ -1,6 +1,30 @@
 using Base: @kwdef
 using CliqueTrees: MF, MMD
 
+"""
+    HyPar(algs;
+        level = 6,
+        width = 120,
+        imbalances = 130:130,
+    )
+
+Hypergraph partitioning based optimizer. Recursively bipartitions a tensor network, then calls a
+greedy algorithm on the leaves. The optimizer is run a number of times: once for each greedy
+algorithm in `algs` and each imbalance value in `imbalances`. The recursion depth is controlled by
+the parameters `level` and `width`.
+
+The optimizer is implemented using the hypergraph partitioning
+library [KaHyPar.jl](https://github.com/kahypar/KaHyPar.jl) and the tree decomposition library
+[CliqueTrees.jl](https://github.com/AlgebraicJulia/CliqueTrees.jl).
+
+# Arguments
+
+  - `algs`: tuple of [elimination algorithms](https://algebraicjulia.github.io/CliqueTrees.jl/stable/api/#Elimination-Algorithms).
+  - `level`: maximum level
+  - `width`: minimum width
+  - `imbalances`: imbalance parameters 
+
+"""
 struct HyPar{A} <: Optimizer
     algs::A
     level::Int
